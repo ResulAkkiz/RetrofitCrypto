89739eb046387b2ca5a3ba487502e11d357ecdfe
Resul Akkız <resulakkiz@gmail.com>
2023-06-30 23:55:41 +0300

    Rxjava(has junkcode)

--- app/src/main/java/com/project/retrofitcryptokotlin/MainActivity.kt
+++ app/src/main/java/com/project/retrofitcryptokotlin/MainActivity.kt
@@ -2,26 +2,26 @@ package com.project.retrofitcryptokotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView.LayoutManager
-import com.project.retrofitcryptokotlin.adapter.CoinRecyclerViewAdapter
 import com.project.retrofitcryptokotlin.databinding.ActivityMainBinding
-import com.project.retrofitcryptokotlin.model.CoinModel
+
 import com.project.retrofitcryptokotlin.model.Rate
 import com.project.retrofitcryptokotlin.service.CryptoAPI
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val baseURL: String = "https://rest.coinapi.io/v1/"
     private var cryptoList = ArrayList<Rate>()
-    private var colorList: Array<String>? =null
+    private var colorList: Array<String>? = null
+    private var compositeDisposable:CompositeDisposable?=null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +31,8 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
         loadData()
-        colorList=resources.getStringArray(R.array.colors)
+        colorList = resources.getStringArray(R.array.colors)
+        compositeDisposable= CompositeDisposable()
         binding.coinRecyclerView.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
 
@@ -40,40 +41,47 @@ class MainActivity : AppCompatActivity() {
     private fun loadData() {
         val retrofit =
             Retrofit.Builder().baseUrl(baseURL).addConverterFactory(GsonConverterFactory.create())
-                .build()
-        val service = retrofit.create(CryptoAPI::class.java)
-        val call = service.getData()
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .build().create(CryptoAPI::class.java)
+        val call = retrofit.getData()
+      /*  compositeDisposable?.add(retrofit.getData()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(this::handleResponse))
+        call.subscribe()*/
 
-        call.enqueue(object : Callback<CoinModel> {
-            override fun onResponse(call: Call<CoinModel>, response: Response<CoinModel>) {
-                if (response.isSuccessful) {
-                    val body = response.body()
-                    body?.let {
-                        if (body.rates != null)
-                            for (singleRate in body.rates!!) {
+        /* call.enqueue(object : Callback<CoinModel> {
+             override fun onResponse(call: Call<CoinModel>, response: Response<CoinModel>) {
+                 if (response.isSuccessful) {
+                     val body = response.body()
+                     body?.let {
+                         if (body.rates != null)
+                             for (singleRate in body.rates!!) {
 
-                                cryptoList.add(singleRate)
-                            }
-                    }
+                                 cryptoList.add(singleRate)
+                             }
+                     }
 
-                }
+                 }
 
-                binding.coinRecyclerView.adapter = CoinRecyclerViewAdapter(cryptoList,colorList!!,object :CoinRecyclerViewAdapter.Listener{
-                    override fun onClick(rate: Rate) {
-                        Toast.makeText(this@MainActivity,rate.assetIdQuote!!,Toast.LENGTH_SHORT).show()
-                    }
+                 binding.coinRecyclerView.adapter = CoinRecyclerViewAdapter(cryptoList,colorList!!,object :CoinRecyclerViewAdapter.Listener{
+                     override fun onClick(rate: Rate) {
+                         Toast.makeText(this@MainActivity,rate.assetIdQuote!!,Toast.LENGTH_SHORT).show()
+                     }
 
-                })
-            }
+                 })
+             }
 
-            override fun onFailure(call: Call<CoinModel>, t: Throwable) {
-                t.printStackTrace()
-            }
+             override fun onFailure(call: Call<CoinModel>, t: Throwable) {
+                 t.printStackTrace()
+             }
 
-        })
+         })*/
 
 
     }
+
+
 
 
 }
--- app/src/main/java/com/project/retrofitcryptokotlin/service/CryptoAPI.kt
+++ app/src/main/java/com/project/retrofitcryptokotlin/service/CryptoAPI.kt
@@ -2,10 +2,12 @@ package com.project.retrofitcryptokotlin.service
 
 import com.project.retrofitcryptokotlin.model.CoinModel
 import com.project.retrofitcryptokotlin.model.Rate
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Observer
 import retrofit2.Call
 import retrofit2.http.GET
 
 interface CryptoAPI {
     @GET("exchangerate/USD?apikey=45C1FDED-8994-4FFD-AF59-70D6024F9564")
-    fun getData():Call<CoinModel>
+    fun getData():Observable<CoinModel>
 }
